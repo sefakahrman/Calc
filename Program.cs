3876d003986a5d6642f058c5c094f8ed37ec81ce
Sefa Kahraman <devsefakahraman@gmail.com>
2024-09-05 15:40:03 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+try
+{
+    Console.WriteLine("**CalC**\n");
+    Console.Write("Enter a number: ");
+    int inputNumber1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("\nSecond Number: ");
+    int inputNumber2 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("\nEnter the Transaction You Want to Perform (* - / +) : ");
+    string operationInput = Console.ReadLine();
+
+    if (!(operationInput == ""))
+    {
+        if (operationInput == "*")
+        {
+            multiplyMethod(inputNumber1, inputNumber2);
+        }
+        else if (operationInput == "-")
+        {
+            minusMethod(inputNumber1, inputNumber2);
+        }
+        else if (operationInput == "+")
+        {
+            plusMethod(inputNumber1, inputNumber2);
+        }
+        else if (operationInput == "/")
+        {
+            divideMethod(inputNumber1, inputNumber2);
+        }
+        else
+        {
+            Console.WriteLine("The operation you selected is undefined...");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Empty value");
+    }
+
+}
+catch (FormatException)
+{
+    Console.WriteLine("You entered a non-recommended value");
+}
+
+
+//Operations Methods
+static void plusMethod(int numberOne, int numberTwo)
+{
+    Console.WriteLine($"\n{numberOne} + {numberTwo} Result: " + (numberOne + numberTwo));
+}
+static void minusMethod(int numberOne, int numberTwo)
+{
+    Console.WriteLine($"\n{numberOne} - {numberTwo} Result: " + (numberOne - numberTwo));
+}
+static void multiplyMethod(int numberOne, int numberTwo)
+{
+    Console.WriteLine($"\n{numberOne} * {numberTwo} Result: " + numberOne * numberTwo);
+}
+static void divideMethod(double numberOne, double numberTwo)
+{
+    Console.WriteLine($"\n{numberOne} / {numberTwo} Result: " + numberOne / numberTwo);
+}
